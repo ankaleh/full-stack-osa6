464a6a5e9c5d2241767da797a02444e7cf2b4f90
Anna Lehto <anviinamaki@gmail.com>
2021-04-27 15:07:40 +0300

    Update README.md.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Full Stack open -kurssin osa 6: Redux-kirjasto
+
+Helsingin yliopiston [Full Stack open -kurssin](https://fullstackopen.com/) tehtävät:
+
+* Redux-kirjasto ([kurssin osaan 6](https://fullstackopen.com/osa6))
--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -26,16 +26,16 @@ const AnecdoteList = () => {
         <div>
             <h2>Anecdotes</h2>
             {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div >
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
+              <div key={anecdote.id}>
+                <div>
+                  {anecdote.content}
+                </div>
+                <div >
+                  has {anecdote.votes}
+                  <button onClick={() => vote(anecdote)}>vote</button>
+                </div>
+              </div>
+            )}
 
         </div>
     )
